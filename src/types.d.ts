c6dd469b211c6bf719432ba50c2827327b6d8d57
recursive-beast <splentercell.1997@gmail.com>
2024-04-08 01:27:37 +0000

    add JSDoc descriptions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,9 @@
 import { ArgumentsCamelCase, BuilderCallback } from 'yargs';
 
+/**
+ * Infers the argument types from a builder callback type.
+ * @template B The builder callback type.
+ * @returns Arguments type, with camelcased keys
+ */
 export type InferArguments<B> =
   B extends BuilderCallback<unknown, infer R> ? ArgumentsCamelCase<R> : never;
--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,11 @@ dayjs.extend(customParseFormat);
 const home = os.homedir();
 const defaultConfigPath = path.join(home, '.config', name, 'config.json');
 
+/**
+ * Reads the configuration from a file.
+ * @param filepath The path to the configuration file.
+ * @returns A Promise that resolves to a `scite.Configuration` instance.
+ */
 export async function readConfig(filepath = defaultConfigPath) {
   filepath = path.resolve(filepath);
 
@@ -22,10 +27,20 @@ export async function readConfig(filepath = defaultConfigPath) {
   return new scite.Configuration({ basePath: 'https://api.scite.ai', ...json });
 }
 
+/**
+ * Checks if an error is an instance of ResponseError.
+ * @param error The error to check.
+ * @returns True if the error is an instance of ResponseError, false otherwise.
+ */
 export function isResponseError(error: unknown): error is scite.ResponseError {
   return error instanceof Error && error.name === 'ResponseError';
 }
 
+/**
+ * Converts a ResponseError to a a user friendly Error object.
+ * @param err The ResponseError to convert.
+ * @returns A new Error object with the error message, status, and body information.
+ */
 export async function fromResponseError(err: scite.ResponseError) {
   const response = err.response;
   const body = await response.json();
@@ -38,6 +53,11 @@ Body: ${JSON.stringify(body, null, 2)}
   return new Error(message);
 }
 
+/**
+ * Creates a parser function for a given Zod schema.
+ * @param schema The Zod schema to use for parsing.
+ * @returns A parser function that validates and parses the input data.
+ */
 export function createParser<T>(schema: z.ZodType<T>) {
   return function parse(data: unknown) {
     try {
@@ -52,6 +72,13 @@ export function createParser<T>(schema: z.ZodType<T>) {
   };
 }
 
+/**
+ * Validates a date string against a given format.
+ * @param date The date string to validate.
+ * @param format The format or an array of formats to validate against.
+ * @throws An error if the date is not in the specified format or is not a valid date.
+ * @returns The validated date string.
+ */
 export function validateDateString(date: string, format: string | string[]) {
   format = Array.isArray(format) ? format : [format];
   const isValid = dayjs(date, format, true).isValid();
