7da1accf693e3aa4057116f9bab09d2a090cc1c3
recursive-beast <splentercell.1997@gmail.com>
2024-04-18 23:09:19 +0100

    refactor papers tests to use test.each

--- tests/papers.test.ts
+++ tests/papers.test.ts
@@ -11,57 +11,38 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-it('should retrieve papers by DOIs', async () => {
-  const doi = ['10.1007/s10616-007-9104-1', '10.1002/bio.4224'];
-  const res = await axios.post('https://api.scite.ai/papers', doi);
+const cases = [
+  {
+    name: 'should retrieve papers by DOIs',
+    args: {
+      doi: ['10.1007/s10616-007-9104-1', '10.1002/bio.4224'],
+      target: false,
+    },
+    fetcher: (doi: string[]) => axios.post('https://api.scite.ai/papers', doi),
+  },
+  {
+    name: 'should filter out papers not found by DOI',
+    args: {
+      doi: ['Tk7jQTRHFU3z3mvES2X9VDjiaqAF6t', '10.1007/s10616-007-9104-1'],
+      target: false,
+    },
+    fetcher: (doi: string[]) => axios.post('https://api.scite.ai/papers', doi),
+  },
+  {
+    name: 'should retrieve papers citing a given DOI',
+    args: {
+      doi: ['10.1007/s10616-007-9104-1'],
+      target: true,
+    },
+    fetcher: (doi: string[]) =>
+      axios.get(`https://api.scite.ai/papers/sources/${doi[0]}`),
+  },
+];
 
-  await handler({ doi, target: false, $0: 'scite-cli', _: ['papers'] });
+it.each(cases)('$name', async ({ args, fetcher }) => {
+  const res = await fetcher(args.doi);
 
-  const calls = spy.mock.calls;
-
-  expect(spy).toHaveBeenCalledTimes(1);
-  expect(calls[0]).toHaveLength(1);
-
-  const arg = calls[0][0];
-
-  expect(typeof arg).toBe('string');
-
-  const data: PapersResponse = res.data;
-
-  const expected = extractIds(Object.values(data.papers));
-  const actual = extractIds(JSON.parse(arg));
-
-  expect(actual).toStrictEqual(expected);
-});
-
-it('should filter out papers not found by DOI', async () => {
-  const doi = ['Tk7jQTRHFU3z3mvES2X9VDjiaqAF6t', '10.1007/s10616-007-9104-1'];
-  const res = await axios.post('https://api.scite.ai/papers', doi);
-
-  await handler({ doi, target: false, $0: 'scite-cli', _: ['papers'] });
-
-  const calls = spy.mock.calls;
-
-  expect(spy).toHaveBeenCalledTimes(1);
-  expect(calls[0]).toHaveLength(1);
-
-  const arg = calls[0][0];
-
-  expect(typeof arg).toBe('string');
-
-  const data: PapersResponse = res.data;
-
-  const expected = extractIds(Object.values(data.papers));
-  const actual = extractIds(JSON.parse(arg));
-
-  expect(actual).toStrictEqual(expected);
-});
-
-it('should retrieve papers citing a given DOI', async () => {
-  const doi = '10.1007/s10616-007-9104-1';
-  const res = await axios.get(`https://api.scite.ai/papers/sources/${doi}`);
-
-  await handler({ doi: [doi], target: true, $0: 'scite-cli', _: ['papers'] });
+  await handler({ ...args, $0: 'scite-cli', _: ['papers'] });
 
   const calls = spy.mock.calls;
 
@@ -80,7 +61,7 @@ it('should retrieve papers citing a given DOI', async () => {
   expect(actual).toStrictEqual(expected);
 });
 
-it("should throw when the doi doesn't exist", async () => {
+it("should throw when targeting a doi that doesn't exist", async () => {
   const promise = handler({
     doi: ['non-existant-doi'],
     target: true,
@@ -91,19 +72,12 @@ it("should throw when the doi doesn't exist", async () => {
   await expect(promise).rejects.toThrow();
 });
 
-it('should throw when DOIs are empty strings', async () => {
+it.each([
+  { doi: ['', ''], target: false },
+  { doi: [''], target: true },
+])('should throw when no DOIs are provided', async (args) => {
   let promise = handler({
-    doi: ['', ''],
-    target: false,
-    $0: 'scite-cli',
-    _: ['papers'],
-  });
-
-  await expect(promise).rejects.toThrow();
-
-  promise = handler({
-    doi: [''],
-    target: true,
+    ...args,
     $0: 'scite-cli',
     _: ['papers'],
   });
